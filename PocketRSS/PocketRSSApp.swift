2b1b418f97068054007cf264a9d6379bf3869589
Ilya Suvorov <vsilux@gmail.com>
2025-05-17 15:03:52 +0300

    initial commit

---
+++ PocketRSS/PocketRSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketRSSApp.swift
+//  PocketRSS
+//
+//  Created by Illia Suvorov on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PocketRSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
